56e20c49cc5aa7b7342c8556713ed9a7059033a1
Roger Salloum <roger.salloum@unbounce.com>
2022-05-12 13:46:21 -0700

    Updating api endpoints to start using objects

--- jeopardy-front-end/public/question.php
+++ jeopardy-front-end/public/question.php
@@ -1,55 +1,28 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/Sources/GoogleSheets.php';
+
 if (!isset($_ENV['GOOGLE_API_KEY']) || $_ENV['GOOGLE_API_KEY'] == '') {
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode(array('error' => 'Missing Google API Key'));
     exit;
+} else if (!isset($_GET['gameId']) || $_GET['gameId'] == '') {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(array('error' => 'Missing gameId parameter'));
+    exit;
+} else if (!isset($_GET['category']) || $_GET['category'] == '') {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(array('error' => 'Missing category parameter'));
+    exit;
+} else if (!isset($_GET['value']) || $_GET['value'] == '') {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(array('error' => 'Missing value parameter'));
+    exit;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
-
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient()
-{
-    $client = new Google_Client();
-    $client->setApplicationName('Jeopardy');
-    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
-    $client->setDeveloperKey($_ENV['GOOGLE_API_KEY']);
-    $client->setAccessType('offline');
-
-    return $client;
-}
-
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Sheets($client);
-
-$question = null;
-
-if (isset($_GET['gameId'])) {
-    $spreadsheetId = $_GET['gameId'];
-    $range = 'Jeopardy Example!A2:C';
-    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-    $values = $response->getValues();
-    
-    
-    
-    foreach ($values as $value) {
-        if (isset($_GET['value']) && $_GET['category'] &&
-            $value[0] == $_GET['value'] && $value[1] == $_GET['category']
-        ) {
-            $question = $value[2];
-            break;
-        }
-    }
-}
-
-// Will want to filter out more than 6 categories or ones that dont have enough questions
-$response = array('question' => $question);
+$source = new Sources\GoogleSheets($_ENV['GOOGLE_API_KEY'], $_GET['gameId']);
+$response = array('answer' => $source->getAnswer($_GET['category'], $_GET['value']));
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($response);
