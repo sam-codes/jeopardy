01464b5dccce23a2fd24a466b88388b526a43b86
Roger Salloum <roger.salloum@unbounce.com>
2022-05-12 16:21:01 -0700

    Adding in the question endpoint to the api

--- jeopardy-front-end/src/components/API/index.js
+++ jeopardy-front-end/src/components/API/index.js
@@ -1,7 +1,20 @@
 //import React from "react";
 
 export const getCategories = async (gameId) => {
-	return fetch("http://localhost/categories?gameId=" + gameId, {
+	return fetch(`http://localhost/categories?gameId=${gameId}`, {
+			method: "GET",
+			headers: {
+				"access-control-allow-origin" : "*",
+				"Content-type": "application/json; charset=UTF-8"
+		}})
+		.then((res) => res.json())
+		.then((json) => {
+			return json.categories
+		})
+};
+
+export const getAnswer = async (gameId, value, category) => {
+	return fetch(`http://localhost/question?gameId=${gameId}&value=${value}&category=${category}`, {
 			method: "GET",
 			headers: {
 				"access-control-allow-origin" : "*",
